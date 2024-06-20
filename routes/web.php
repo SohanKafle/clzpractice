f3b532341728b4671a85fcef6196c0c787f04465
SohanKafle <soka_bca2077@lict.edu.np>
2024-06-20 10:00:51 +0545

    commit

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,12 @@ Route::get('/products/{id}/delete',[ProductsController::class,'delete'])->name('
 
 
 
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
